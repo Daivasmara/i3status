23dce5dce99edd0f773579368800271cc228cb73
Muhammad Daivasmara Denaw <daivasmara.denaw@vidio.com>
2021-01-31 11:08:44 +0700

    replace locale with float string literal

--- i3status/crypto.py
+++ i3status/crypto.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 import json
 import urllib.request
-import locale
-
-locale.setlocale(locale.LC_ALL, 'en_US.utf-8')
 
 with urllib.request.urlopen("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,lto-network,nimiq,fantom&vs_currencies=usd") as url:
     data = json.loads(url.read().decode())
-    str = "₿ " + locale.currency(data["bitcoin"]["usd"], grouping=True) + "  "
-    str += "⟠  " + locale.currency(data["ethereum"]["usd"], grouping=True) + "  "
-    str += "⚶ " + locale.currency(data["lto-network"]["usd"], grouping=True) + "  "
-    str += "⎔ " + locale.currency(data["nimiq"]["usd"], grouping=True) + "  "
-    str += "❏ " + locale.currency(data["fantom"]["usd"], grouping=True)
+    str = "₿ " + "${:,.0f}".format(data["bitcoin"]["usd"]) + "  "
+    str += "⟠  " + "${:,.0f}".format(data["ethereum"]["usd"]) + "  "
+    str += "⚶ " + "${:,.3f}".format(data["lto-network"]["usd"]) + "  "
+    str += "⎔ " + "${:,.3f}".format(data["nimiq"]["usd"]) + "  "
+    str += "❏ " + "${:,.3f}".format(data["fantom"]["usd"])
     print(str, end='')
