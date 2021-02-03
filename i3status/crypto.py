7f15bfb6c322d3e595c3fa158ea32efd34af1c85
Muhammad Daivasmara Denaw <daivasmara.denaw@vidio.com>
2021-02-03 09:05:55 +0700

    added zero to cryptos

--- i3status/crypto.py
+++ i3status/crypto.py
@@ -2,11 +2,12 @@
 import json
 import urllib.request
 
-with urllib.request.urlopen("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,lto-network,nimiq,fantom&vs_currencies=usd") as url:
+with urllib.request.urlopen("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,lto-network,nimiq,fantom,zero-exchange&vs_currencies=usd") as url:
     data = json.loads(url.read().decode())
     str = "₿ " + "${:,.0f}".format(data["bitcoin"]["usd"]) + "  "
     str += "⟠  " + "${:,.0f}".format(data["ethereum"]["usd"]) + "  "
     str += "⚶ " + "${:,.3f}".format(data["lto-network"]["usd"]) + "  "
     str += "⎔ " + "${:,.3f}".format(data["nimiq"]["usd"]) + "  "
-    str += "❏ " + "${:,.3f}".format(data["fantom"]["usd"])
+    str += "  " + "${:,.3f}".format(data["fantom"]["usd"]) + "  "
+    str += "𝚫  " + "${:,.3f}".format(data["zero-exchange"]["usd"])
     print(str, end='')
