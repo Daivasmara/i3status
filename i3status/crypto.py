240264d6dfbc54ac8fc9ece134c37b5611cbd5a4
Muhammad Daivasmara Denaw <daivasmara.denaw@vidio.com>
2021-02-09 12:48:31 +0700

    added serum and injective protocol

--- i3status/crypto.py
+++ i3status/crypto.py
@@ -2,12 +2,12 @@
 import json
 import urllib.request
 
-with urllib.request.urlopen("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,lto-network,nimiq,fantom,zero-exchange&vs_currencies=usd") as url:
+with urllib.request.urlopen("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,lto-network,nimiq,fantom,serum,injective-protocol&vs_currencies=usd") as url:
     data = json.loads(url.read().decode())
     str = "₿ " + "${:,.0f}".format(data["bitcoin"]["usd"]) + "  "
     str += "⟠  " + "${:,.0f}".format(data["ethereum"]["usd"]) + "  "
     str += "⚶ " + "${:,.3f}".format(data["lto-network"]["usd"]) + "  "
-    str += "⎔ " + "${:,.3f}".format(data["nimiq"]["usd"]) + "  "
     str += "  " + "${:,.3f}".format(data["fantom"]["usd"]) + "  "
-    str += "𝚫  " + "${:,.3f}".format(data["zero-exchange"]["usd"])
+    str += " " + "${:,.3f}".format(data["serum"]["usd"]) + "  "
+    str += " " + "${:,.3f}".format(data["injective-protocol"]["usd"])
     print(str, end='')
